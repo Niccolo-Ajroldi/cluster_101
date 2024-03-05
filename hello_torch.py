d4b59c4019a603668b70cc2663c55bb4668a8252
Niccolò Ajroldi <61059403+Niccolo-Ajroldi@users.noreply.github.com>
2024-03-05 14:56:53 +0100

    Update hello_torch.py

--- hello_torch.py
+++ hello_torch.py
@@ -10,16 +10,7 @@ def print_cuda_info():
     print("CUDA is available")
     num_gpus = torch.cuda.device_count()
     print("Number of GPUs:", num_gpus)
-
-    #if torch.distributed.get_rank() == 0:
-    #    for i in range(num_gpus):
-    #        gpu_info = torch.cuda.get_device_properties(i)
-    #        print(f"GPU {i}:")
-    #        print(f"  Name: {gpu_info.name}")
-    #        print(f"  CUDA Capability: {gpu_info.major}.{gpu_info.minor}")
-    #        print(f"  Total Memory: {gpu_info.total_memory / (1024 ** 3):.2f} GB")
-    #        print(f"  CUDA Cores: {gpu_info.multi_processor_count * 64}")
-
+    
 if __name__ == "__main__":
     # Print CUDA information
     print("=================")
